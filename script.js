e982b08a700b09a842382781a5a7f7a292e9373c
granthonaker <gahonaker@gmail.com>
2023-08-04 19:05:56 -0400

    add a delete button for done items

--- script.js
+++ script.js
@@ -4,9 +4,19 @@ const ul = document.querySelector("ul");
 
 const inputLength = () => input.value.length;
 
+//new list items
 const createListElement = () => {
 	let li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
+	// creates delete button
+	let btn = document.createElement('button');
+	btn.innerHTML = "Done";
+	li.appendChild(btn);
+	//removes element
+	btn.addEventListener('click', () => {
+		li.parentNode.removeChild(li);
+	})
+
 	ul.appendChild(li);
 	input.value = "";
 }
